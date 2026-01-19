7c9345f5c9b207e688bf1fb2a4f686470154465f
gabriel aparecido mizael <gabri@NITROV15>
2026-01-19 17:02:25 -0300

    First project and folder structure

---
+++ FormacaoDotNET/1DesvendeOsFundamentos/FundamentosDoCSharp/Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("First value: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(v1);
+        }
+    }
+}
+
+
