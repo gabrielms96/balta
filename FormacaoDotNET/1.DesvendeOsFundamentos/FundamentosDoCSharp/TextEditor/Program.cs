a45bb7fc99ffacd257421ef1e7b14884da143204
gabriel aparecido mizael <gabri@NITROV15>
2026-01-21 22:49:30 -0300

    Initial commit: basic C# text editor project setup

---
+++ FormacaoDotNET/1.DesvendeOsFundamentos/FundamentosDoCSharp/TextEditor/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace TextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1 - Open File");
+            Console.WriteLine("2 - Create New File");
+            Console.WriteLine("0 - Exit");
+            Console.WriteLine("-----------------------");
+            short option = short.Parse(Console.ReadLine());
+            switch (option)
+            {
+                case 0: Environment.Exit(0); break;
+                case 1: OpenFile(); break;
+                case 2: Edit(); break;
+                default: Menu(); break;
+            }
+        }
+
+        static void OpenFile()
+        {
+
+        }
+
+        static void Edit()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter your text below (Press ESC to finish):");
+            Console.WriteLine("----------------------------------------------");
+            string text = "";
+            do
+            {
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Console.Write(text);
+        }
+    }
+}
